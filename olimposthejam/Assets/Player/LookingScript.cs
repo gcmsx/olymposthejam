ffde84bff3b814e89b4f2e3cc87179528050a4a0
gcmsx <gcmsx3@gmail.com>
2023-12-08 19:26:02 +0300

    inital 4

---
+++ olimposthejam/Assets/Player/LookingScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StapeliaGames{
+
+public class LookingScript : MonoBehaviour {
+    public float sensitivity = 5.0f;
+    Vector2 mouseLook;
+    GameObject character;
+    public static bool can_look;
+    
+    void Start() {
+        character = this.transform.parent.gameObject;
+        EnableLook();
+        LockCursor();
+    }
+
+    void Update() {
+        if(can_look) {
+            var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+            md = Vector2.Scale(md, new Vector2(sensitivity, sensitivity));
+            mouseLook += md;
+            mouseLook.y = Mathf.Clamp(mouseLook.y, -75, 75);
+            transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+            character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+        }
+    }
+
+    public void DisableLook() {
+        can_look = false;
+    }
+
+    public void EnableLook() {
+        can_look = true;
+    }
+
+    public void LockCursor() {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void UnlockCursor() {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
+}
---
+++ olimposthejam/Assets/Player/MovementScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace StapeliaGames{
+
+public class MovementScript : MonoBehaviour {
+    public float speed = 10.0F;
+    public static bool can_control;
+    Rigidbody rb;
+    
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        EnableMovement();
+    }
+
+    void Update() {
+        Vector3 velocety = rb.velocity;
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, 2f);
+
+        if(can_control) {
+            if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+                rb.AddRelativeForce(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime, ForceMode.Impulse);
+            else rb.velocity = Vector3.zero;
+            if(Input.GetKeyDown(KeyCode.Space)) {
+                rb.AddRelativeForce(0, 20, 0, ForceMode.Impulse);
+            }
+        }
+        else {
+            rb.velocity = Vector3.zero;
+        }
+    }
+
+    public void DisableMovement() { 
+        can_control = false;
+    }
+
+    public void EnableMovement() { 
+        can_control = true;
+    }
+
+}
+}
